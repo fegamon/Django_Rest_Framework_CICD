5bc23f1879ad94a522b5f4416e6f01144e66f7d4
fegamon <fegamon1999@gmail.com>
2025-03-08 22:52:09 -0500

    Added flake8

--- To_Do/todo/views.py
+++ To_Do/todo/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from todo.models import Task
 from todo.serializers import TaskSerializer
 
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
