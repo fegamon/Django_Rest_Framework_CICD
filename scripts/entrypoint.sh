93cadcea64835cca95dfe75ae5e14bd0137f4929
fegamon <fegamon1999@gmail.com>
2025-03-18 01:04:42 -0500

    Finished initial dev and production environments

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,12 +4,14 @@ echo "Applying migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
 
 if [ "$ENVIRONMENT" = "production" ]; then
+    echo "Collecting static files..."
+    mkdir -p staticfiles
+    python manage.py collectstatic --noinput
+
     echo "Starting production server..."
-    exec gunicorn your_project_name.wsgi:application --bind 0.0.0.0:8000
+    exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
 else
     echo "Starting development server..."
     exec python manage.py runserver 0.0.0.0:8000
